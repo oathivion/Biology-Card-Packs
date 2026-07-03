12ba9d01d2b000a60125b8ba3691268892fabc1d
oathivion <brian.thomas.ritchie@gmail.com>
2026-07-03 11:17:28 -0700

    Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.wilddecks.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "2"
+        versionCode = 3
+        versionName = "3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true

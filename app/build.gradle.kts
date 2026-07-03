535397e6317f6b114dea6ec51f3233d53e0d42da
oathivion <brian.thomas.ritchie@gmail.com>
2026-07-02 22:24:25 -0700

    -

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.wilddecks.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = .1
+        versionCode = 0.1
         versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
